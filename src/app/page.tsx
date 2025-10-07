ade40caa9431957988c0fecbad1eb473707ff887
Marcelo Hadweh Briceño <marcelo.hadwehb@gmail.com>
2025-10-07 10:48:39 +0000

    Try fixing this error: `Runtime Error: Error: Failed to load chunk /_nex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { FinancesDashboard } from '@/components/finances/FinancesDashboard';
+import DashboardLoader from '@/components/finances/DashboardLoader';
 
 export default function Home() {
   return (
     <main className="flex items-start justify-center min-h-screen p-4 sm:p-6 md:p-8">
-      <FinancesDashboard />
+      <DashboardLoader />
     </main>
   );
 }
