663645c3b06f52731cf6ac9aa694cc2a9e99a542
Marcelo Hadweh Briceño <marcelo.hadwehb@gmail.com>
2025-10-08 16:13:48 +0000

    Try fixing this error: `Runtime Error: Error: Failed to load chunk /_nex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,6 @@
 'use client';
 
-import dynamic from 'next/dynamic';
-import { Skeleton } from '@/components/ui/skeleton';
-
-const FinancesDashboard = dynamic(
-  () => import('@/components/finances/FinancesDashboard').then(mod => mod.FinancesDashboard),
-  {
-    ssr: false,
-    loading: () => (
-      <div className="w-full max-w-2xl mx-auto p-4 sm:p-6 md:p-8">
-        <Skeleton className="h-24 w-full mb-6" />
-        <Skeleton className="h-12 w-full mb-6" />
-        <Skeleton className="h-48 w-full" />
-      </div>
-    ),
-  }
-);
+import { FinancesDashboard } from '@/components/finances/FinancesDashboard';
 
 export default function Home() {
   return (
